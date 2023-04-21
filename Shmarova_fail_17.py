31978a6fab2a56f862fe5b7b5e0939f1c3e453b6
XD26 <anasmarova@gmail.com>
2023-04-21 21:43:09 +0300

    перезапись файла с транспонированной матрицей

---
+++ Shmarova_fail_17.py
@@ -0,0 +1,37 @@
+'''
+Написать функцию считывания размерности 𝑛 и матрицы из файла InputMatrix.txt
+в двумерный список 𝐿. Далее матрица транспонируется и выводится в файл
+OutputMatrix.txt, имеющий такую же структуру, как и входной файл.
+'''
+
+fp = open('InputMatrix.txt', 'r')
+razm = fp.readline().strip('\n')
+mt = []
+s = fp.readline().strip('\n')
+s = s.split()
+mt.append(s)
+while s:
+    s = fp.readline().strip('\n')
+    s = s.split()
+    mt.append(s)
+mt = mt[:-1]
+fp.close()
+
+print(mt)
+print(razm)
+
+fp = open('OutputMatrix.txt','w')
+fp.write(str(razm) + '\n')
+
+i = 0
+while i < int(razm):
+    j = 0
+    while j < int(razm):
+        fp.write(mt[j][i] + ' ')
+        j +=1
+    fp.write('\n')
+    i += 1
+
+fp.close()
+
+
