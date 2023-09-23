0e55c58c4c1b1ab071e12290e793f910bb6b7948
Trefex-trefex <misha.friday@mail.ru>
2023-09-23 15:12:00 +0300

    лаба 2

--- lab2/UnitTest1/UnitTest1.cpp
+++ lab2/UnitTest1/UnitTest1.cpp
@@ -1,6 +1,7 @@
 #include "pch.h"
 #include "CppUnitTest.h"
-#include "../lab2/digits.h"
+#include "../lab2.1/Header.h"
+
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -10,21 +11,53 @@ namespace UnitTest1
 	{
 	public:
 		
-		TEST_METHOD(TestDigits1)
+		TEST_METHOD(TestDistance1)
 		{
-			Assert::AreEqual(digits(23), 2);
+			Assert::AreEqual(Distance(6, 6, 3, 2), 5.);
 		}
-		TEST_METHOD(TestDigits2)
+		TEST_METHOD(TestDistance2)
 		{
-			Assert::AreEqual(digits(5), 1);
+			Assert::AreEqual(Distance(-2, 2, -2, 6), 4.);
 		}
-		TEST_METHOD(TestDigits3)
+		TEST_METHOD(TestDistance3)
 		{
-			Assert::AreEqual(digits(0), 1);
+			Assert::AreEqual(Distance(2, 0, -1, -4), 5.);
 		}
-		TEST_METHOD(TestDigits4)
+		TEST_METHOD(TestDistance4)
 		{
-			Assert::AreEqual(digits(-15), 2);
+			Assert::AreEqual(Distance(3, 3, 3, 3), 0.);
+		}
+		TEST_METHOD(TestDistanceToLine1)
+		{
+			Assert::AreEqual(DistanceToLine(4, 4, 6, 1, 6, 5), 2.);
+		}
+		TEST_METHOD(TestDistanceToLine2)
+		{
+			Assert::AreEqual(DistanceToLine(-1, 5, -3, 2, 1, 2), 3.);
+		}
+		TEST_METHOD(TestDistanceToLine3)
+		{
+			Assert::AreEqual(DistanceToLine(0, 1, 2, -2, 6, -2), 3.);
+		}
+		TEST_METHOD(TestDistanceToLine4)
+		{
+			Assert::AreEqual(DistanceToLine(2, 4, 5, 4, 2, 1), 2.121, 0.001);
+		}
+		TEST_METHOD(TestSquare1)
+		{
+			Assert::AreEqual(Square(1, 1, 5, 1, 2, 4), 6., 0.0001);
+		}
+		TEST_METHOD(TestSquare2)
+		{
+			Assert::AreEqual(Square(1, 1, 1, 1, 1, 1), 0.);
+		}
+		TEST_METHOD(TestSquare3)
+		{
+			Assert::AreEqual(Square(1, 1, 5, 1, 3, 1), 0.);
+		}
+		TEST_METHOD(TestSquare4)
+		{
+			Assert::AreEqual(Square(1, 1, 4, 5, 8, 5), 8.);
 		}
 	};
 }
