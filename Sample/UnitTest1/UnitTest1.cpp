88c46d364826dc44b6e31437abf4311cba714afd
Trefex-trefex <misha.friday@mail.ru>
2023-10-05 10:58:57 +0300

    шаблон пустого проекта с тестами

---
+++ Sample/UnitTest1/UnitTest1.cpp
@@ -0,0 +1,30 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "..\\Sample\Header.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			Assert::AreEqual(, );
+		}
+		TEST_METHOD(TestMethod2)
+		{
+			Assert::AreEqual(, );
+		}
+		TEST_METHOD(TestMethod3)
+		{
+			Assert::AreEqual(, );
+		}
+		TEST_METHOD(TestMethod4)
+		{
+			Assert::AreEqual(, );
+		}
+	};
+}
